21fa05c85f69b776771e141e5be1cc682b3d422d
edwinaguilarariza <edwinaguilarariza1@yahoo.com>
2021-05-21 16:31:35 -0500

    creacion header y footer con enrutamiento

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { RouterModule, Routes } from '@angular/router';  
+import { HomeComponent } from './components/home/home.component';
+import { StoreComponent } from './components/store/store.component' ;
+import { AboutUsComponent } from './components/about-us/about-us.component' ;
+import { ContactComponent } from './components/contact/contact.component' ;
+import { MyAccountComponent } from './components/my-account/my-account.component' ;
+import { CartComponent } from './components/cart/cart.component' ;
+
+
+const APP_ROUTES: Routes = [
+    { path: 'home', component: HomeComponent  },
+    { path: 'store', component: StoreComponent  },
+    { path: 'aboutUs', component: AboutUsComponent  },
+    { path: 'contact', component: ContactComponent  },
+    { path: 'myAccount', component: MyAccountComponent  },
+    { path: 'cart', component: CartComponent  },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+
+];
+
+
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES , {useHash:true});
